2df2b1d0a80c62f0add81163475bed6612b78b06
talmeym <mtalmey@gmail.com>
2015-10-12 11:52:36 +0100

    rebuilt repo

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# regurgitator-extensions-web-xml
-regurgitator extensions web xml
+# regurgitator-extensions-web-config-xml
+Regurgitator is a simple extendable processing engine, ideal for regurgitating canned or clever response to incoming requests. This component allows the configuration of the framework extensions for the web using xml.
---
+++ src/main/java/com/emarte/regurgitator/extensions/web/HttpCallThroughXmlLoader.java
@@ -0,0 +1,34 @@
+package com.emarte.regurgitator.extensions.web;
+
+import com.emarte.regurgitator.core.*;
+import org.dom4j.Element;
+
+import java.util.Set;
+
+import static com.emarte.regurgitator.core.XmlConfigUtil.*;
+import static com.emarte.regurgitator.extensions.web.WebConfigConstants.*;
+import static java.lang.Integer.parseInt;
+
+public class HttpCallThroughXmlLoader implements XmlLoader<Step> {
+    private static Log log = Log.getLog(HttpCallThroughXmlLoader.class);
+	private static final XmlLoaderUtil<XmlLoader<Step>> loaderUtil = new XmlLoaderUtil<XmlLoader<Step>>();
+
+    @Override
+    public Step load(Element element, Set<Object> allIds) throws RegurgitatorException {
+        String id = loadId(element, allIds);
+
+		String host = loadValueFromElementOrAttribute(element, HOST);
+		String port = loadValueFromElementOrAttribute(element, PORT);
+
+		Element processResponseElement = element.element(PROCESS_RESPONSE);
+		Step responseProcessing = null;
+
+		if(processResponseElement != null) {
+			Element innerElement = getChild(processResponseElement);
+			responseProcessing = loaderUtil.deriveLoader(innerElement).load(innerElement, allIds);
+		}
+
+		log.debug("Loaded HttpCallThrough '" + id + "'");
+        return new HttpCallThrough(id, new HttpMessageProxy(host, parseInt(port)), responseProcessing);
+    }
+}

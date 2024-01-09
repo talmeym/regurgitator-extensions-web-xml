4fc80f1791f8309f0bc3614909f7b5ed2d0317ba
talmeym <mtalmey@gmail.com>
2024-01-09 14:11:39 +0000

    emarte uk

--- src/main/java/uk/emarte/regurgitator/extensions/web/CreateHttpResponseXmlLoader.java
+++ src/main/java/uk/emarte/regurgitator/extensions/web/CreateHttpResponseXmlLoader.java
@@ -2,19 +2,19 @@
  * Copyright (C) 2017 Miles Talmey.
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  */
-package com.emarte.regurgitator.extensions.web;
+package uk.emarte.regurgitator.extensions.web;
 
-import com.emarte.regurgitator.core.CreateResponse;
-import com.emarte.regurgitator.core.CreateResponseXmlLoader;
-import com.emarte.regurgitator.core.RegurgitatorException;
-import com.emarte.regurgitator.core.XmlLoader;
 import org.w3c.dom.Element;
+import uk.emarte.regurgitator.core.CreateResponse;
+import uk.emarte.regurgitator.core.CreateResponseXmlLoader;
+import uk.emarte.regurgitator.core.RegurgitatorException;
+import uk.emarte.regurgitator.core.XmlLoader;
 
 import java.util.Set;
 
-import static com.emarte.regurgitator.core.XmlConfigUtil.loadOptionalStr;
-import static com.emarte.regurgitator.extensions.web.ExtensionsWebConfigConstants.CONTENT_TYPE;
-import static com.emarte.regurgitator.extensions.web.ExtensionsWebConfigConstants.STATUS_CODE;
+import static uk.emarte.regurgitator.core.XmlConfigUtil.loadOptionalStr;
+import static uk.emarte.regurgitator.extensions.web.ExtensionsWebConfigConstants.CONTENT_TYPE;
+import static uk.emarte.regurgitator.extensions.web.ExtensionsWebConfigConstants.STATUS_CODE;
 
 public class CreateHttpResponseXmlLoader implements XmlLoader<CreateHttpResponse> {
     private final CreateResponseXmlLoader createResponseXmlLoader = new CreateResponseXmlLoader();
